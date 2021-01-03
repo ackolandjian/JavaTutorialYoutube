afc9e2de455108113a31f2b9034e4525a22d3295
Anna Christiane Kolandjian <51375362+ackolandjian@users.noreply.github.com>
2021-01-03 17:55:28 +0100

    Add files via upload

---
+++ NamingConventions/src/namingConvention/Main.java
@@ -0,0 +1,14 @@
+package namingConvention;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 5;
+		int myAge = 5;
+		String bookTitle = "Java for Beginners";
+		String a = "Java for Beginners";
+		
+	}
+
+}
---
+++ NamingConventions/src/namingConvention/Naming.java
@@ -0,0 +1,13 @@
+package namingConvention;
+
+public class Naming {
+	String name;
+	Naming(String name){
+		this.name = name;
+	}
+	
+	public String returnName() {
+		return name;
+	}
+	
+}
